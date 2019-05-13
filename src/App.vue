340d61d3121399780fe18daf9cb4f104531046f0
Alvaro AVGS. Gonzalez Serrano <alvaro.gonzalez@stkproj.com>
2019-05-13 15:22:55 +0200

    First version

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <template>
-  <div id="app"></div>
+  <div id="app" class="app">
+    <modal v-if="modalShow && modal === 'modal-test'" @close="closeModal" title="Modal with title">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dictum dignissim fermentum. Vestibulum ac rutrum elit,
+        <br>
+        <br>dignissim vulputate massa. Donec nunc mi, condimentum at scelerisque et, cursus pretium sapien. Proin viverra interdum eleifend. Sed fermentum felis augue, id porttitor ante imperdiet sed.
+      </p>
+    </modal>
+
+    <modal v-if="modalShow && modal === 'modal-test-two'" @close="closeModal">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dictum dignissim fermentum. Vestibulum ac rutrum elit,</p>
+    </modal>
+    <p @click.prevent="openModal('modal-test')">Open!</p>
+    <p @click.prevent="openModal('modal-test-two')">Open modal-test-two!</p>
+  </div>
 </template>
 
 <script>
@@ -8,9 +22,36 @@ import Modal from "./components/Modal.vue";
 export default {
   name: "app",
   components: {
-    Modal: "v-modal"
+    Modal
+  },
+
+  data: function() {
+    return {
+      modalShow: false,
+      modal: null
+    };
+  },
+  methods: {
+    openModal(modal) {
+      this.modalShow = true;
+      this.modal = modal;
+    },
+    closeModal() {
+      this.modalShow = false;
+      this.modal = null;
+    }
   }
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+@import "styles/index";
+
+.app {
+  min-height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: $lightGrey;
+}
+</style>
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,10 +1,93 @@
-<template></template>
+<template>
+  <div class="c-modal">
+    <section class="c-modal__wrap">
+      <font-awesome-icon icon="times-circle" @click="closeModal" class="c-modal__wrap__close"/>
+
+      <header v-if="title" class="c-modal__header">
+        <h2 class="c-modal__header__title">{{ title }}</h2>
+      </header>
+
+      <main class="c-modal__content">
+        <slot></slot>
+      </main>
+
+      <footer></footer>
+    </section>
+  </div>
+</template>
 
 <script>
 export default {
-  name: "Modal"
+  name: "Modal",
+  props: {
+    title: {
+      type: String,
+      required: false
+    }
+  },
+  created() {
+    if (!this.$slots.default) {
+      console.error("Modal component requires content be provided in the slot.");
+    }
+  },
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    }
+  }
 };
 </script>
 
-<style scoped lang="scss">
+<style  lang="scss">
+.c-modal {
+  height: 100vh;
+  width: 100%;
+  padding: 30px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  position: absolute;
+  left: 0;
+  top: 0;
+  background-color: rgba($darkGrey, 0.2);
+
+  &__wrap {
+    background-color: white;
+    position: relative;
+    border-radius: 2px;
+    box-shadow: 5px 5px rgba($darkGrey, 0.2);
+    max-width: 960px;
+
+    &__close {
+      position: absolute;
+      right: -15px;
+      top: -15px;
+
+      font-size: 3rem;
+      cursor: pointer;
+      transition: 0.3s all ease;
+      color: $red;
+
+      &:hover {
+        color: $darkRed;
+      }
+    }
+  }
+
+  &__header {
+    padding: 15px 30px;
+    background-color: $lightGrey;
+
+    &__title {
+      font-size: 2.2rem;
+      font-weight: 700;
+    }
+  }
+
+  &__content {
+    padding: 30px;
+  }
+}
 </style>
